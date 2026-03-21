ee609f593041cefcf1ac8882b03f492da3ec26c6
Esteban Soto <esoto074@gmail.com>
2026-03-20 21:08:18 -0600

    Apply review fix: count only published posts in tags index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,13 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.left_joins(:posts)
-               .group(:id)
-               .select("tags.*, COUNT(posts.id) AS posts_count")
-               .order(:name)
+    @tags = Tag
+      .joins("LEFT JOIN post_tags ON post_tags.tag_id = tags.id
+              LEFT JOIN posts ON posts.id = post_tags.post_id
+                AND posts.status = #{Post.statuses[:published]}
+                AND posts.published_at <= NOW()")
+      .group("tags.id")
+      .select("tags.*, COUNT(posts.id) AS posts_count")
+      .order(:name)
   end
 
   def show
