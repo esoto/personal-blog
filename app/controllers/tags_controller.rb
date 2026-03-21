e6fa47b4056f90d1ad7c3b720fc197889ba17209
Esteban Soto <esoto074@gmail.com>
2026-03-20 21:10:50 -0600

    Apply review fix: eager load rich text body to prevent N+1

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by!(slug: params[:slug])
-    @posts = @tag.posts.published.recent.includes(:tags)
+    @posts = @tag.posts.published.recent.includes(:tags).with_rich_text_body
   end
 end
