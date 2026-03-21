c8ea76a64a4bfa35deecf9efe41ccad64c79039d
Esteban Soto <esoto074@gmail.com>
2026-03-21 12:12:05 -0600

    Fix indentation in nav helper and add trix editor min-height

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def nav_link_class(path, prefix_match: false)
     active = if prefix_match
                request.path == path || request.path.start_with?("#{path}/")
-             else
+    else
                current_page?(path)
-             end
+    end
     if active
       "text-accent-blue font-semibold transition-fast"
     else
